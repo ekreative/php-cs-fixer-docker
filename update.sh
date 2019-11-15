bfa8b58fd3afc283269a3a11b15516c9446ecab6
Fred Cox <mcfedr@gmail.com>
2019-11-15 12:08:09 +0200

    Fix cs :)

--- update.sh
+++ update.sh
@@ -4,9 +4,9 @@ set -e
 # https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases
 declare -A variants=(
   ['2']='2.16.0'
-	['2.16']='2.16.0'
-	['2.15']='2.15.0'
-	['1']='1.13.2'
+  ['2.16']='2.16.0'
+  ['2.15']='2.15.0'
+  ['1']='1.13.2'
 )
 
 for variant in "${!variants[@]}"; do
